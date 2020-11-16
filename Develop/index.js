be6a67d80db93d6e3dbedbd2cbadeedf49d1815b
Dustin Pidcock <dustygen@gmail.com>
2020-11-16 13:36:41 -0800

    added some code to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,33 @@
-// array of questions for user
+//for external files
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+//questions for the user
 const questions = [
+    {
+        type: 'input',
+        message: "What would you like to name this project?",
+        name: 'name', 
+    },
+    {
+        type: 'input',
+        message: "Please decribe what this project is.",
+        name: 'description', 
+    },
+    {
+        type: 'input',
+        message: "What is the application supposed to do?",
+        name: 'purpose'
+    },
+    {
+        type: 'input',
+        message: "How do you install it?",
+        name: 'Install',
+    },
+    {
+        type
+    }
 
 ];
 
